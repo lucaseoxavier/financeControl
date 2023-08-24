063c58bac73e682ad327aab369a0eac9d55e2fa8
Lucas Xavier <lucaseoxavier@gmail.com>
2023-08-23 23:50:07 -0300

    project setup

---
+++ src/test/java/com/smd/financeControl/FinanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smd.financeControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
